cb1a4c7a5b02a4b4189ba4890e3f84463331241f
Eryk Wójcik <101266606+pananananas@users.noreply.github.com>
2024-01-03 01:24:07 +0100

    restyling the tweets page

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  images: {
+    domains: ["images.clerk.dev"],
+  },
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
